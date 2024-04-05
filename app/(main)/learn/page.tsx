5bea4811ed4086aed439eac3e94c4fd2e93c767b
DmitriyAngve <kirirch129@gmail.com>
2024-04-05 15:25:59 +0300

    feat: Page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,25 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+
+import { Header } from "./header";
+
 const LearnPage = () => {
-  return <div>Learn Page</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
