84e91b7521378bfaa9c9af905763716e8e73a524
DmitriyAngve <kirirch129@gmail.com>
2024-04-09 16:37:28 +0300

    feat: user progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,10 +1,22 @@
+import { redirect } from "next/navigation";
+
+import { getUserProgress } from "@/db/queries";
+
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
 
 import { Header } from "./header";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickWrapper>
--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,32 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
   title: text("title"),
   imageSrc: text("image_src").notNull(),
 });
+
+// Еще одна таблица
+export const coursesRelations = relations(courses, ({ many }) => ({
+  userProgress: many(userProgress),
+}));
+
+export const userProgress = pgTable("user_progress", {
+  userId: text("user_id").primaryKey(), // primaryKey - для уникальности каждого пользователя
+  userName: text("user_name").notNull().default("User"),
+  userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
+  activeCourseId: integer("active_course_id").references(() => courses.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(5), // представляет собой целочисленное поле, не может быть null, по умолчанию равно 5
+  points: integer("points").notNull().default(0),
+});
+
+// relations - создает связь между таблицами в БД. В моем случае она опеределяет отношения между courses/userProgress/coursesRelations
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+  activeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  }),
+}));
