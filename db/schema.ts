b8d97a7e2e9b691673d0e038f87a31452c147990
DmitriyAngve <kirirch129@gmail.com>
2024-04-10 16:43:41 +0300

    feat: schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,13 @@
 import { relations } from "drizzle-orm";
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+import {
+  boolean,
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+} from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
@@ -10,7 +18,114 @@ export const courses = pgTable("courses", {
 // Еще одна таблица
 export const coursesRelations = relations(courses, ({ many }) => ({
   userProgress: many(userProgress),
+  units: many(units), // courses have many units, but each unit can only have one course relation
 }));
+
+export const units = pgTable("units", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(), // Unit 1
+  description: text("description").notNull(), // Learn the basics of spanish
+  courseId: integer("course_id")
+    .references(() => courses.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  order: integer("order").notNull(), // Мне надо знать как это рендерить
+});
+
+// Relations with the units
+export const unitsRelations = relations(units, ({ many, one }) => ({
+  course: one(courses, {
+    fields: [units.courseId],
+    references: [courses.id],
+  }),
+}));
+
+// lessons
+export const lessons = pgTable("lessons", {
+  id: serial("id").primaryKey(), // первичный ключ, который инкрементируется
+  title: text("title").notNull(), // заголовок, не может быть null
+  unitId: integer("unit_id") // идентификатор учебного блока, к которому относится урок. Это внешний ключ, который ссылается на id в таблице units. При его удалении, остальные смещаются по каскаду.
+    .references(() => units.id, { onDelete: "cascade" })
+    .notNull(), // each lesson needs to have only one unitId. I need relation for that
+  order: integer("order").notNull(), // порядковый номер урока
+});
+
+export const lessonsRelations = relations(lessons, ({ one, many }) => ({
+  unit: one(units, {
+    fields: [lessons.id],
+    references: [units.id],
+  }),
+  challenges: many(challenges),
+})); // это ф-ия, которая создает связи между таблицами lessons and units
+
+// type of a challenges
+export const challengesEnum = pgEnum("type", ["SELECT", "ASSIST"]);
+
+export const challenges = pgTable("challenges", {
+  id: serial("id").primaryKey(),
+  lessonId: integer("lesson_id")
+    .references(() => lessons.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  type: challengesEnum("type").notNull(),
+  question: text("question").notNull(),
+  order: integer("order").notNull(), // чтобы не было случайного порядка рендера
+});
+
+export const challengesRelations = relations(challenges, ({ one, many }) => ({
+  lesson: one(lessons, {
+    fields: [challenges.lessonId],
+    references: [lessons.id],
+  }),
+  challengeOptions: many(challengeOptions), // eache challenge can have a lot of options but each individual challengeOption can only have one challenge
+  challengeProgress: many(challengeProgress),
+}));
+
+export const challengeOptions = pgTable("challenge_options", {
+  id: serial("id").primaryKey(),
+  challengeId: integer("challenge_id")
+    .references(() => challenges.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  text: text("text").notNull(),
+  correct: boolean("correct").notNull(),
+  imageSrc: text("image_src"),
+  audioSrc: text("audio_src"), // аудио не обязательно для пользователя
+});
+
+export const challengeOptionsRelations = relations(
+  challengeOptions,
+  ({ one }) => ({
+    challenge: one(challenges, {
+      fields: [challengeOptions.challengeId],
+      references: [challenges.id],
+    }),
+  })
+);
+
+export const challengeProgress = pgTable("challenge_progress", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull(), // TODO: Confirm this doesn't breake
+  challengeId: integer("challenge_id")
+    .references(() => challenges.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  completed: boolean("completed").notNull().default(false), // так я буду следит за прогрессом пользователя.
+});
+
+export const challengeProgressRelations = relations(
+  challengeProgress,
+  ({ one }) => ({
+    challenge: one(challenges, {
+      fields: [challengeProgress.challengeId],
+      references: [challenges.id],
+    }),
+  })
+);
 
 export const userProgress = pgTable("user_progress", {
   userId: text("user_id").primaryKey(), // primaryKey - для уникальности каждого пользователя
