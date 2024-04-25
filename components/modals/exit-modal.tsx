4435b9975ada6d2ab86027608b8f52711084ed32
DmitriyAngve <kirirch129@gmail.com>
2024-04-25 12:27:11 +0300

    feat: exit modal window

--- components/modals/exit-modal.tsx
+++ components/modals/exit-modal.tsx
@@ -15,4 +15,56 @@ import {
 import { Button } from "@/components/ui/button";
 import { useExitModal } from "@/store/use-exit-modal";
 
-export const ExitModal = () => {};
+export const ExitModal = () => {
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+  const { isOpen, close } = useExitModal();
+
+  useEffect(() => setIsClient(true), []);
+
+  // to avoid suffer with potential hydration error, I use this method
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={close}>
+      <DialogContent className="max-w-md">
+        <DialogHeader>
+          <div className="flex items-center w-full justify-center mb-5">
+            <Image src="/mascot_sad.svg" alt="Mascot" height={80} width={80} />
+          </div>
+          <DialogTitle className="text-center font-bold text-2xl">
+            Wait, don&apos;t go!
+          </DialogTitle>
+          <DialogDescription className="text-center text-base">
+            You&apos;re about to leave the lesson. Are your sure?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="mb-4">
+          <div className="flex flex-col gap-4 w-full">
+            <Button
+              variant="primary"
+              className="w-full"
+              size="lg"
+              onClick={close}
+            >
+              Keep learning
+            </Button>
+            <Button
+              variant="dangerOutline"
+              className="w-full"
+              size="lg"
+              onClick={() => {
+                close();
+                router.push("/learn");
+              }}
+            >
+              End session
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
